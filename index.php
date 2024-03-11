0a5d82763653667072c4933a2884e6f1cd2e7655
Navneet Yadav <nav700neet@gmail.com>
2024-03-11 12:10:48 +0530

    new changes-2

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 
            
    
+<?php
+include "config.php";
+session_start();
+
+if(isset( $_SESSION["username"] ))
+{
+    header('Location: http://localhost:8080/new-website/main.php');
+}
+
+?>
 
 
 <!DOCTYPE html>
